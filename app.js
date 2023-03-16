86dbb9993fb59aff1ca9779ce66640db48b22ffc
Kristina <krisew10@gmail.com>
2023-03-16 10:13:27 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-<form action="FirstPage.html">
+{/* <form action="FirstPage.html">
     <button type="button" href="kristina.html" class="btn">Basic</button>
-</form>
+</form> */}
+
+// function StartFunction() {
+//     var T = document.getElementById("start"),
+//         displayValue = "";
+//     if (T.style.display == "")
+//         displayValue = "none";
+
+//     T.style.display = displayValue;
+// }
